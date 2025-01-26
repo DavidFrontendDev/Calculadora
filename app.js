cadcb7d8407bc1931dd2b79ac530ff6273efbd79
DavidFrontendDev <daviddevbackend@gmail.com>
2025-01-26 21:17:41 +0100

    Funcionalidad acabada de JS

---
+++ app.js
@@ -0,0 +1,24 @@
+const pantalla = document.querySelector("input");
+const botones = document.querySelectorAll("button");
+botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    let valorBoton = boton.textContent;
+    try {
+      if (valorBoton === "0" && pantalla.value === "") {
+        return; // Saltamos este caso.
+      } else if (valorBoton === "," && pantalla.value === "") {
+        return; // Saltamos este caso.
+      } else if (valorBoton === "AC") {
+        pantalla.value = "";
+      } else if (valorBoton === "←") {
+        pantalla.value = pantalla.value.slice(0, -1);
+      } else if (valorBoton === "=") {
+        pantalla.value = eval(pantalla.value);
+      } else {
+        pantalla.value += valorBoton;
+      }
+    } catch {
+      pantalla.value = "Error";
+    }
+  });
+});
